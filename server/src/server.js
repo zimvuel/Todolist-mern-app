80ad0d11035a41e5a258aa0c76f5176dccf79d17
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2026-01-17 19:12:50 +0700

    feat: add docker and used nginx to serve frontend

--- server/src/server.js
+++ server/src/server.js
@@ -6,8 +6,13 @@ import connectDB from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
