b5f24c30b8ace6ddd0af3c30fc538430d46228f0
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2026-06-04 10:23:54 +0700

    feat(client): update page title, favicon and logo assets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,8 @@ import { ThemeProvider } from "./context/ThemeProvider"
 const App = () => {
   return (
     <ThemeProvider>
-      <div className="flex h-screen flex-col items-center justify-center">
-        <main>
-          <Outlet/>
-        </main>
+      <div className="min-h-screen">
+        <Outlet/>
       </div>
     </ThemeProvider>
     
